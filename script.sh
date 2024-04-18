82d654e3b4e33c612f3d9f5c1281ce909c6ea46f
dipanshu <medipansh@gmail.com>
2024-04-18 22:48:40 +0530

    update

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REF=HEAD
+SINCE=origin/main
+DIR_TO_CHECK=svc-1
+# DIR_TO_CHECK=svc-2
+
+MERGE_BASE=$(git merge-base ${SINCE} ${REF})
+echo $MERGE_BASE
+FILES_CHANGED=$(git diff --name-only ${MERGE_BASE}..${REF} -- ${DIR_TO_CHECK})
+printf "Files changed:\n${FILES_CHANGED}\n"
+
+if [[ -n $FILES_CHANGED ]]; then
+  echo "Found changes !!! ... setting var to true"
+#   echo 'export IS_CHANGED='"true" >> "$BASH_ENV"
+else
+  echo "No changes Found!!! ... setting var to false"
+#   echo 'export IS_CHANGED='"false" >> "$BASH_ENV"
+fi
